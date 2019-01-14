046c0e1b8adf71c0bbcaa7827a066aaed62be458
Shaleen Jain <shaleen@jain.sh>
2019-01-14 16:00:02 +0530

    use lowercase variables

--- test.sh
+++ test.sh
@@ -2,23 +2,23 @@
 set -e
 set -o pipefail
 
-ERRORS=()
+errors=()
 
 # find all executables and run `shellcheck`
-for f in $(find . -type f -not -iwholename '*.git*' -not -iwholename '*.tmux*' -not -name "yubitouch.sh" | sort -u); do
+for f in $(find . -type f -not -path '*.git*' -not -iwholename '*.tmux*' -not -name "yubitouch.sh" | sort -u); do
 	if file "$f" | grep --quiet shell; then
 		{
 			shellcheck "$f" && echo "[OK]: sucessfully linted $f"
 		} || {
 			# add to errors
-			ERRORS+=("$f")
+			errors+=("$f")
 		}
 	fi
 done
 
-if [ ${#ERRORS[@]} -eq 0 ]; then
+if [ ${#errors[@]} -eq 0 ]; then
 	echo "No errors, hooray"
 else
-	echo "These files failed shellcheck: ${ERRORS[*]}"
+	echo "These files failed shellcheck: ${errors[*]}"
 	exit 1
 fi
