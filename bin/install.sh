c66ab43276fdaa1bf5340417ab3a10d73ed9d0e0
Shaleen Jain <shaleen@jain.sh>
2023-10-31 10:21:00 +0530

    update neovim ppa again

--- bin/install.sh
+++ bin/install.sh
@@ -47,7 +47,10 @@ setup_sources_min() {
 		--no-install-recommends
 
 	# neovim
-	add-apt-repository ppa:neovim-ppa/stable
+	cat <<-EOF > /etc/apt/sources.list.d/neovim.list
+	deb http://ppa.launchpad.net/neovim-ppa/unstable/ubuntu jammy main
+	deb-src http://ppa.launchpad.net/neovim-ppa/unstable/ubuntu jammy main
+	EOF
 
 	# add the neovim ppa gpg key
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 9DBB0BE9366964F134855E2255F96FCF8231B6DD
